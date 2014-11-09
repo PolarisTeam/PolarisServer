40b63d1907347cbc7642a84bd9df776d520fcf7d
Sean CyberKitsune McClenaghan <cyberkitsune09@gmail.com>
2014-11-09 07:53:52 -0800

    Split off Login, cleaned up some stuff.

--- PolarisServer/Client.cs
+++ PolarisServer/Client.cs
@@ -97,6 +97,7 @@ namespace PolarisServer
 		void HandleConnectionLost()
 		{
 			// :(
+			Console.WriteLine ("[:( ] :(");
 		}
 
 
@@ -131,107 +132,15 @@ namespace PolarisServer
 			Console.WriteLine ("[-->] Packet {0:X}-{1:X} ({2} bytes)", typeA, typeB, size);
 
 
-			if (typeA == 0x11 && typeB == 0) {
-				HandleLogin (data, position, size);
-			} else {
-				Packets.Handlers.PacketHandler handler = Packets.Handlers.PacketHandlers.getHandlerFor (typeA, typeB);
-				if (handler != null)
-					handler.handlePacket (this, data, position, size);
-				else
-					Console.WriteLine ("[!!!] UNIMPLEMENTED PACKET");
-				//throw new NotImplementedException ();
-			}
+
+			Packets.Handlers.PacketHandler handler = Packets.Handlers.PacketHandlers.getHandlerFor (typeA, typeB);
+			if (handler != null)
+				handler.handlePacket (this, data, position, size);
+			else
+				Console.WriteLine ("[!!!] UNIMPLEMENTED PACKET");
+			//throw new NotImplementedException ();
 		}
-
-
-		// PSO2 encryption junk, just because
-		/*
-		void HandleKeyExchange (byte[] data, uint position, uint size)
-		{
-			if (size < 0x80)
-				return;
-
-			// Extract the first 0x80 bytes into a separate array
-			var cryptedBlob = new byte[0x80];
-			Array.Copy (data, position, cryptedBlob, 0, 0x80);
-			Array.Reverse (cryptedBlob);
-
-			// FIXME
-			if (_rsaCsp == null) {
-				_rsaCsp = new RSACryptoServiceProvider ();
-				var rsaBlob = System.IO.File.ReadAllBytes ("privateKey.blob");
-				_rsaCsp.ImportCspBlob (rsaBlob);
-			}
-
-			var pkcs = new RSAPKCS1KeyExchangeDeformatter (_rsaCsp);
-			byte[] decryptedBlob = null;
-
-			try {
-				decryptedBlob = pkcs.DecryptKeyExchange (cryptedBlob);
-			} catch (CryptographicException) {
-				// Failed. Should probably drop the connection here.
-				// TODO ?
-				return;
-			}
-
-			// Also a failure.
-			if (decryptedBlob.Length < 0x20)
-				return;
-
-
-			// Extract the RC4 key
-			var arc4Key = new byte[16];
-			Array.Copy (decryptedBlob, 0x10, arc4Key, 0, 0x10);
-
-			// Create three RC4 mungers
-			var arc4 = new Mono.Security.Cryptography.ARC4Managed ();
-			arc4.Key = arc4Key;
-			_inputARC4 = arc4.CreateDecryptor ();
-
-			arc4 = new Mono.Security.Cryptography.ARC4Managed ();
-			arc4.Key = arc4Key;
-			_outputARC4 = arc4.CreateEncryptor ();
-
-			arc4 = new Mono.Security.Cryptography.ARC4Managed ();
-			arc4.Key = arc4Key;
-			var tempDecryptor = arc4.CreateDecryptor ();
-
-			// Also, grab the init token for the client
-			var decryptedToken = new byte[16];
-			tempDecryptor.TransformBlock (decryptedBlob, 0, 0x10, decryptedToken, 0);
-
-			SendPacket (0x11, 0xC, 0, decryptedToken);
-		}
-		*/
-
-
-
-		void HandleLogin(byte[] data, uint position, uint size)
-		{
-			// Mystery packet
-			var mystery = new Packets.Writer ();
-			mystery.Write ((uint)100);
-			//SendPacket (0x11, 0x49, 0, mystery.ToArray ());
-
-			// Login response packet
-			var resp = new Packets.Writer ();
-			resp.Write ((uint)0); // Status flag: 0=success, 1=error
-			resp.WriteUTF16 ("This is an error", 0x8BA4, 0xB6);
-			resp.Write ((uint)200); // Player ID
-			resp.Write ((uint)0); // Unknown
-			resp.Write ((uint)0); // Unknown
-			resp.WriteFixedLengthUTF16 ("B001-DarkFox", 0x20);
-			for (int i = 0; i < 0xBC; i++)
-				resp.Write ((byte)0);
-			SendPacket (0x11, 1, 4, resp.ToArray ());
-
-			// Settings packet
-			var settings = new Packets.Writer ();
-			settings.WriteASCII (
-				System.IO.File.ReadAllText ("settings.txt"),
-				0x54AF, 0x100);
-			SendPacket (0x2B, 2, 4, settings.ToArray ());
-		}
+						
 	}
 }
 
---
+++ PolarisServer/Packets/Handlers/Login.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace PolarisServer.Packets.Handlers
+{
+	[PacketHandlerAttr(0x11, 0x0)]
+	public class Login : PacketHandler
+	{
+		public Login ()
+		{
+		}
+
+		public override void handlePacket (Client context, byte[] data, uint position, uint size)
+		{
+			// Mystery packet
+			var mystery = new Packets.Writer ();
+			mystery.Write ((uint)100);
+			//SendPacket (0x11, 0x49, 0, mystery.ToArray ());
+
+			// Login response packet
+			var resp = new Packets.Writer ();
+			resp.Write ((uint)0); // Status flag: 0=success, 1=error
+			resp.WriteUTF16 ("This is an error", 0x8BA4, 0xB6);
+			resp.Write ((uint)200); // Player ID
+			resp.Write ((uint)0); // Unknown
+			resp.Write ((uint)0); // Unknown
+			resp.WriteFixedLengthUTF16 ("B001-DarkFox", 0x20);
+			for (int i = 0; i < 0xBC; i++)
+				resp.Write ((byte)0);
+			context.SendPacket (0x11, 1, 4, resp.ToArray ());
+
+			// Settings packet
+			var settings = new Packets.Writer ();
+			settings.WriteASCII (
+				System.IO.File.ReadAllText ("settings.txt"),
+				0x54AF, 0x100);
+			context.SendPacket (0x2B, 2, 4, settings.ToArray ());
+		}
+	}
+}
+
