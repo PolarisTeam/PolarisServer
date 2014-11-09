9d3954a8432eba115627e09fc5c506c3f230e277
Sean CyberKitsune McClenaghan <cyberkitsune09@gmail.com>
2014-11-09 01:19:28 -0800

    Relfect all the things

---
+++ PolarisServer/Packets/Handlers/KeyExchange.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace PolarisServer.Packets.Handlers
+{
+	[PacketHandlerAttr(0x11, 0xB)]
+	public class KeyExchange : PacketHandler
+	{
+		public KeyExchange ()
+		{
+		}
+			
+		public override void handlePacket(Client context, byte[] data,  uint position, uint size)
+		{
+		
+		}
+	}
+}
+
---
+++ PolarisServer/Packets/Handlers/PacketHandler.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Reflection;
+using System.Linq;
+
+namespace PolarisServer.Packets.Handlers
+{
+	public class PacketHandlerAttr : Attribute
+	{
+		public uint type, subtype;
+		public PacketHandlerAttr(uint type, uint subtype)
+		{
+			this.type = type;
+			this.subtype = subtype;
+		}
+
+
+	}
+
+
+	public abstract class PacketHandler
+	{
+		public abstract void handlePacket(Client context, byte[] data,  uint position, uint size);
+	}
+
+	public static class PacketHandlers
+	{
+		public static PacketHandler getHandlerFor(uint typeA, uint typeB)
+		{
+			var classes = from t in Assembly.GetExecutingAssembly ().GetTypes ()
+					where t.IsClass && t.Namespace == "PolarisServer.Packets.Handlers" && t.IsSubclassOf (typeof(PacketHandler))
+			              select t;
+			foreach (Type t in classes.ToList()) 
+			{
+				Attribute[] attrs = (Attribute[])t.GetCustomAttributes (typeof(PacketHandlerAttr), false);
+				foreach (Attribute attr in attrs) 
+				{
+					if (attr is PacketHandlerAttr) 
+					{
+						PacketHandlerAttr handlerAttr = (PacketHandlerAttr)attr;
+						if (handlerAttr.type == typeA && handlerAttr.subtype == typeB)
+							return (PacketHandler)Activator.CreateInstance (t);
+					}
+				}
+			}
+
+
+			return null;
+
+		}
+	}
+}
+
