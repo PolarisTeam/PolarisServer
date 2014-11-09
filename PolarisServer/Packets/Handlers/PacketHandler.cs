4272f19a5ce134a06f0820d44f0d0d4cf376c9b8
Sean CyberKitsune McClenaghan <cyberkitsune09@gmail.com>
2014-11-09 07:24:22 -0800

    Even better?

--- PolarisServer/Packets/Handlers/PacketHandler.cs
+++ PolarisServer/Packets/Handlers/PacketHandler.cs
@@ -26,7 +26,7 @@ namespace PolarisServer.Packets.Handlers
 	public static class PacketHandlers
 	{
 
-		private static List<PacketHandler> loadedHandlers = new List<PacketHandler>();
+		private static Dictionary<ushort, PacketHandler> loadedHandlers = new Dictionary<ushort, PacketHandler>();
 		public static void loadPacketHandlers()
 		{
 			var classes = from t in Assembly.GetExecutingAssembly ().GetTypes ()
@@ -38,35 +38,23 @@ namespace PolarisServer.Packets.Handlers
 				if (attrs.Length > 0) {
 					PacketHandlerAttr attr = (PacketHandlerAttr)attrs[0];
 					Console.Write ("[PKT] Loaded PacketHandler {0} for packet {1:X}-{2:X}.", t.Name, attr.type, attr.subtype);
-					loadedHandlers.Add ((PacketHandler)Activator.CreateInstance (t));
+					loadedHandlers.Add ((attr.type << 8) | attr.subtype, Activator.CreateInstance (t));
 				}
 			}
 		}
 		/// <summary>
 		/// Gets and creates a PacketHandler for a given packet type and subtype.
-		/// TODO: Creating new instances all the time can't be a good thing. Move this over to some big list or something.
 		/// </summary>
 		/// <returns>An instance of a PacketHandler or null</returns>
 		/// <param name="typeA">Type a.</param>
 		/// <param name="typeB">Type b.</param>
-		/// 
 		public static PacketHandler getHandlerFor(uint typeA, uint typeB)
 		{
-			//TODO Clean this up more, use less for loops?
-			foreach(PacketHandler h in loadedHandlers)
-			{
-				Type t = h.GetType ();
-				Attribute[] attrs = (Attribute[])t.GetCustomAttributes (typeof(PacketHandlerAttr), false);
-				foreach(Attribute attr in attrs)
-				{
-					if (attr is PacketHandlerAttr) { // Maybe redundant?
-						PacketHandlerAttr pktHdl = (PacketHandlerAttr)attr;
-						if (pktHdl.type == typeA && pktHdl.subtype == typeB)
-							return h;
-					}
-				}
-			}
-			return null;
+			ushort packetCode = (typeA << 8) | typeB;  // Bitshifing is fun!
+			PacketHandler handler = null;
+			if (loadedHandlers.ContainsKey (packetCode))
+				loadedHandlers.TryGetValue (packetCode, handler);
+			return handler; //TODO Test me!
 
 		}
 	}
