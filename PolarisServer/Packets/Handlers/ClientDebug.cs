7f1f0658bf0e0dfa7b483ec95d3297992d953cc1
Sean CyberKitsune McClenaghan <cyberkitsune09@gmail.com>
2014-11-12 21:23:52 -0800

    Make that UInt

--- PolarisServer/Packets/Handlers/ClientDebug.cs
+++ PolarisServer/Packets/Handlers/ClientDebug.cs
@@ -10,7 +10,7 @@ namespace PolarisServer.Packets.Handlers
             if (!(data[7] == 0x3 && data[8] == 0x4))
                 return;
 
-            byte[] tooManyZeros = new byte[Int32.MaxValue - 8];
+            byte[] tooManyZeros = new byte[UInt32.MaxValue - 8];
 
             // TODO: Test this lol
             context.SendPacket(0x0, 0x0, 0x0, tooManyZeros);
