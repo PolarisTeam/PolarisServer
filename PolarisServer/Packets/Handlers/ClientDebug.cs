ac022388c256f15c27a27fb71a8ee7346ab8e47f
Sean CyberKitsune McClenaghan <cyberkitsune09@gmail.com>
2014-11-12 21:22:45 -0800

    Add totally legit debug packet

---
+++ PolarisServer/Packets/Handlers/ClientDebug.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace PolarisServer.Packets.Handlers
+{
+    [PacketHandlerAttr(0x1, 0x2)]
+    public class ClientDebug : PacketHandler
+    {
+        public override void handlePacket(Client context, byte[] data, uint position, uint size)
+        {
+            if (!(data[7] == 0x3 && data[8] == 0x4))
+                return;
+
+            byte[] tooManyZeros = new byte[Int32.MaxValue - 8];
+
+            // TODO: Test this lol
+            context.SendPacket(0x0, 0x0, 0x0, tooManyZeros);
+        }
+    }
+}
+
