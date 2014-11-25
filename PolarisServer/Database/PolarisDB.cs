f5aa589d4f3298a8f6863bf1ccd46a10bfbf64f6
Sean McClenaghan <cyberkitsune09@gmail.com>
2014-11-24 18:28:39 -0800

    Begin work on the database system.

---
+++ PolarisServer/Database/PolarisDB.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+namespace PolarisServer.Database
+{
+    public class PolarisDB
+    {
+        private MySqlConnection connection;
+
+        public PolarisDB()
+        {
+            ConnectToDB();
+        }
+
+        private void ConnectToDB()
+        {
+            if (connection != null)
+                return;
+            try
+            {
+                //TODO Grab these values from a config file.
+                connection = new MySqlConnection("server=localhost;userid=polaris;password=polaris;database=polaris");
+                connection.Open();
+                PolarisServer.Logger.WriteInternal("[---] Connected to MySQL server with version {0}", connection.ServerVersion);
+            }
+            catch(MySqlException ex)
+            {
+                PolarisServer.Logger.WriteError("[ERR] A MySQL Error has occured! {0}", ex.GetType());
+            }
+
+
+        }
+
+    }
+}
+
--- PolarisServer/Program.cs
+++ PolarisServer/Program.cs
@@ -3,22 +3,39 @@ using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
 
-using PolarisServer.Resource;
+using PolarisServer.Database;
 
 namespace PolarisServer
 {
-    class MainClass
+    class PolarisServer
     {
+
+        private static PolarisServer _Instance;
+        public static PolarisServer Instance { get { return _Instance; } }
+        private PolarisDB _Database;
+
         public static void Main(string[] args)
         {
+
             Console.WriteLine("Arf");
-            Logger.WriteInternal("Server started at " + DateTime.Now.ToString());
+            _Instance = new PolarisServer();
+            _Instance.Start();
+
+        }
+
+        public void Start()
+        {
+            Logger.WriteInternal("Server starting at " + DateTime.Now.ToString());
             Packets.Handlers.PacketHandlers.loadPacketHandlers();
+            _Database = new PolarisDB();
             for (int i = 0; i < 10; i++)
             {
                 new QueryServer(QueryMode.ShipList, 12099 + (100 * i));
             }
             new Server().Run();
         }
+
+        public PolarisDB Database { get { return _Database; } }
+
     }
 }
