f35fd10aea7857bc2e38d30ae3fa98231c0667c3
Sean CyberKitsune McClenaghan <cyberkitsune09@gmail.com>
2014-11-25 00:25:45 -0800

    More SQL Architecture stuff... RFC @Ninji @Kyle

---
+++ PolarisServer/Database/DBCharacter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+using PolarisServer;
+using PolarisServer.Models;
+
+namespace PolarisServer.Database
+{
+    public class DBCharacter : DBObject
+    {
+        public static Character GetCharacter()
+        {
+            return null;
+        }
+
+        public override void CreateTables(MySqlConnection connection)
+        {
+            new MySqlCommand("CREATE TABLE IF NOT EXISTS Characters(CharacterID INTEGER NOT NULL, PlayerID INTEGER NOT NULL, " +
+                "Name TEXT, JobParam BLOB, LooksParam BLOB, PRIMARY KEY(CharacterID), FOREIGN KEY (PlayerID) REFRENCES Players(PlayerID))", connection).ExecuteNonQuery();
+        }
+
+    }
+}
+
---
+++ PolarisServer/Database/DBObject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+namespace PolarisServer.Database
+{
+    public abstract class DBObject
+    {
+        public DBObject()
+        {
+        }
+
+        public abstract void CreateTables(MySqlConnection connection);
+    }
+}
+
