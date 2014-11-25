0f6da3bcd151b146fc25158b5efaba926564a20c
Sean CyberKitsune McClenaghan <cyberkitsune09@gmail.com>
2014-11-25 00:05:37 -0800

    More sql stuff

--- PolarisServer/Database/PolarisDB.cs
+++ PolarisServer/Database/PolarisDB.cs
@@ -22,8 +22,22 @@ namespace PolarisServer.Database
                 //TODO Grab these values from a config file.
                 connection = new MySqlConnection("server=localhost;userid=polaris;password=polaris;database=polaris");
                 connection.Open();
+
+                GenerateTables();
+
                 PolarisServer.Logger.WriteInternal("[---] Connected to MySQL server with version {0}", connection.ServerVersion);
-                dbRevision = Convert.ToString(new MySqlCommand("SELECT data FROM dbInfo WHERE name = \"Revision\"", connection).ExecuteScalar());
+                //dbRevision = Convert.ToString(new MySqlCommand("SELECT data FROM ServerInfo WHERE name = \"Revision\"", connection).ExecuteScalar());
+
+                object revObj = GetServerInfo("Revision");
+
+                if(revObj == null)
+                {
+                    revObj = 0;
+                    AddServerinfo("Revision", revObj);
+                }
+
+                dbRevision = Convert.ToString(revObj);
+
                 PolarisServer.Logger.WriteInternal("[---] MySQL server has base data revision {0} loaded.", dbRevision);
 
             }
@@ -35,6 +49,44 @@ namespace PolarisServer.Database
 
         }
 
+        private void GenerateTables()
+        {
+            new MySqlCommand("CREATE TABLE IF NOT EXISTS ServerInfo(name TEXT NOT NULL PRIMARY KEY, data BLOB)", connection).ExecuteNonQuery();
+
+        }
+                            
+        public void AddServerinfo(string name, object value)
+        {
+            try
+            {
+                MySqlCommand cmd = new MySqlCommand("REPLACE INTO ServerInfo(name, data) VALUES(@name, @data)", connection);
+                cmd.Parameters.AddWithValue("name", name);
+                cmd.Parameters.AddWithValue("data", value);
+                
+                cmd.ExecuteNonQuery();
+            }
+            catch (MySqlException ex)
+            {
+                PolarisServer.Logger.WriteError("[ERR] A MySQL Error has occured! {0}: {1}", ex.GetType(), ex.ToString());
+            }
+        }
+
+        public object GetServerInfo(string name)
+        {
+            try
+            {
+                MySqlCommand cmd = new MySqlCommand("SELECT data FROM ServerInfo WHERE name = @name", connection);
+                cmd.Parameters.AddWithValue("name", name);
+                
+                return cmd.ExecuteScalar();
+            }
+            catch (MySqlException ex)
+            {
+                PolarisServer.Logger.WriteError("[ERR] A MySQL Error has occured! {0}: {1}", ex.GetType(), ex.ToString());
+            }
+
+            return null;
+        }
     }
 }
 
