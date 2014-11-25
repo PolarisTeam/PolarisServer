5c0d6e1d501e43d8b57d1a4f904e816d8693f1f7
Sean McClenaghan <cyberkitsune09@gmail.com>
2014-11-24 19:04:17 -0800

    More DB stuff

--- PolarisServer/Database/PolarisDB.cs
+++ PolarisServer/Database/PolarisDB.cs
@@ -6,6 +6,7 @@ namespace PolarisServer.Database
     public class PolarisDB
     {
         private MySqlConnection connection;
+        private string dbRevision;
 
         public PolarisDB()
         {
@@ -22,10 +23,13 @@ namespace PolarisServer.Database
                 connection = new MySqlConnection("server=localhost;userid=polaris;password=polaris;database=polaris");
                 connection.Open();
                 PolarisServer.Logger.WriteInternal("[---] Connected to MySQL server with version {0}", connection.ServerVersion);
+                dbRevision = Convert.ToString(new MySqlCommand("SELECT data FROM dbInfo WHERE name = \"Revision\"", connection).ExecuteScalar());
+                PolarisServer.Logger.WriteInternal("[---] MySQL server has base data revision {0} loaded.", dbRevision);
+
             }
             catch(MySqlException ex)
             {
-                PolarisServer.Logger.WriteError("[ERR] A MySQL Error has occured! {0}", ex.GetType());
+                PolarisServer.Logger.WriteError("[ERR] A MySQL Error has occured! {0}: {1}", ex.GetType(), ex.ToString());
             }
 
 
