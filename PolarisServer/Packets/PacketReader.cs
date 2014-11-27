89cf8721e052febd8042fc96f2f1ae5f076f50be
Sean CyberKitsune McClenaghan <cyberkitsune09@gmail.com>
2014-11-26 21:23:59 -0800

    begin work on packetreader

---
+++ PolarisServer/Packets/PacketReader.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace PolarisServer.Packets
+{
+    public class PacketReader : BinaryReader
+    {
+        public PacketReader(Stream s) : base(s)
+        {
+        }
+
+        public PacketReader(byte[] bytes) : base(new MemoryStream(bytes))
+        {
+        }
+
+        public unsafe T ReadStruct<T>() where T : struct
+        {
+            byte[] structBytes = ReadBytes(Marshal.SizeOf(typeof(T)));
+
+            return Helper.ByteArrayToStructure<T>(structBytes);
+        }
+    }
+}
+
