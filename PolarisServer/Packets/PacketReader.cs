0a01e16d3c35f65976a9b09484d3cef0b415b13c
Treeki <treeki@gmail.com>
2014-11-28 06:04:09 +0100

    Flesh out the PacketReader, make ReadStruct more robust

--- PolarisServer/Packets/PacketReader.cs
+++ PolarisServer/Packets/PacketReader.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Text;
 using System.Runtime.InteropServices;
 
 namespace PolarisServer.Packets
@@ -14,9 +15,86 @@ namespace PolarisServer.Packets
         {
         }
 
+        public PacketReader(byte[] bytes, uint position, uint size) : base(new MemoryStream(bytes, (int)position, (int)size))
+        {
+        }
+
+        public uint ReadMagic(uint xor, uint sub)
+        {
+            return (ReadUInt32() ^ xor) - sub;
+        }
+
+        public string ReadASCII(uint xor, uint sub)
+        {
+            uint magic = ReadMagic(xor, sub);
+
+            if (magic == 0)
+            {
+                return "";
+            }
+            else
+            {
+                uint charCount = magic - 1;
+                uint padding = 4 - (charCount & 3);
+
+                byte[] data = ReadBytes((int)charCount);
+                for (int i = 0; i < padding; i++)
+                    ReadByte();
+
+                return Encoding.ASCII.GetString(data);
+            }
+        }
+
+        public string ReadFixedLengthASCII(uint charCount)
+        {
+            byte[] data = ReadBytes((int)charCount);
+            string str = Encoding.ASCII.GetString(data);
+
+            int endAt = str.IndexOf('\0');
+            if (endAt == -1)
+                return str;
+            else
+                return str.Substring(0, endAt);
+        }
+
+        public string ReadUTF16(uint xor, uint sub)
+        {
+            uint magic = ReadMagic(xor, sub);
+
+            if (magic == 0)
+            {
+                return "";
+            }
+            else
+            {
+                uint charCount = magic - 1;
+                uint padding = (charCount & 1);
+
+                byte[] data = ReadBytes((int)(charCount * 2));
+                ReadUInt16();
+                if (padding != 0)
+                    ReadUInt16();
+
+                return Encoding.GetEncoding("UTF-16").GetString(data);
+            }
+        }
+
+        public string ReadFixedLengthUTF16(int charCount)
+        {
+            byte[] data = ReadBytes(charCount * 2);
+            string str = Encoding.GetEncoding("UTF-16").GetString(data);
+
+            int endAt = str.IndexOf('\0');
+            if (endAt == -1)
+                return str;
+            else
+                return str.Substring(0, endAt);
+        }
+
         public unsafe T ReadStruct<T>() where T : struct
         {
-            byte[] structBytes = ReadBytes(Marshal.SizeOf(typeof(T)));
+            byte[] structBytes = new byte[Marshal.SizeOf(typeof(T))];
+            Read(structBytes, 0, structBytes.Length);
 
             return Helper.ByteArrayToStructure<T>(structBytes);
         }
