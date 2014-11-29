0cc57194425aba2c2e5007a83e6472de67d3d512
Sean CyberKitsune McClenaghan <cyberkitsune09@gmail.com>
2014-11-29 00:50:54 -0800

    Forgot the HI!

--- PolarisServer/Network/SocketServer.cs
+++ PolarisServer/Network/SocketServer.cs
@@ -44,7 +44,7 @@ namespace PolarisServer.Network
                     if (socket == listener.Server)
                     {
                         // New connection
-                        Logger.WriteInternal("[NEW] New connection!");
+                        Logger.WriteInternal("[HI!] New connection!");
 
                         SocketClient c = new SocketClient(this, listener.AcceptTcpClient());
 
