77a17add6fc98aa86bf64898619b1b838d2ed15d
Sean CyberKitsune McClenaghan <cyberkitsune09@gmail.com>
2014-11-29 00:43:38 -0800

    Move Character list

---
+++ PolarisServer/Packets/Handlers/CharacterList.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+namespace PolarisServer.Packets.Handlers
+{
+    [PacketHandlerAttr(0x11, 2)]
+    public class RequestCharacterList : PacketHandler
+    {
+        public override void handlePacket(Client context, byte[] data, uint position, uint size)
+        {
+            if (context.User == null)
+                return;
+
+            var db = PolarisApp.Instance.Database;
+            var chars = from c in db.Characters
+                    where c.Player.PlayerID == context.User.PlayerID
+                select c;
+
+            PacketWriter writer = new PacketWriter();
+            writer.Write((uint) chars.Count());
+
+            foreach (var ch in chars)
+            {
+                writer.Write((uint)ch.CharacterID);
+                writer.Write((uint)context.User.PlayerID);
+                for (int i = 0; i < 0xC; i++)
+                    writer.Write((byte)0);
+
+                writer.WriteFixedLengthUTF16(ch.Name, 16);
+                writer.Write((uint)0);
+
+                writer.WriteStruct(ch.Looks);
+                writer.WriteStruct(ch.Jobs);
+                for (int i = 0; i < 0x44; i++)
+                    writer.Write((byte)0);
+            }
+
+            // Ninji note: This packet may be followed by extra data,
+            // after a fixed-length array of character data structures.
+            // Needs more investigation at some point.
+
+            context.SendPacket(0x11, 0x3, 0x0, writer.ToArray());
+        }
+    }
+}
+
