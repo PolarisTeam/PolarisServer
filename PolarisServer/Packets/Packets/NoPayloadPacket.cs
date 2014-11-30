a5784a7860449148472c62debc01f81664c34a32
Sean CyberKitsune McClenaghan <cyberkitsune09@gmail.com>
2014-11-29 17:51:55 -0800

    Add NoPayloadPacket

---
+++ PolarisServer/Packets/Packets/NoPayloadPacket.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace PolarisServer.Packets
+{
+    public class NoPayloadPacket : Packet
+    {
+        byte type, subtype;
+
+        public NoPayloadPacket(byte type, byte subtype)
+        {
+        }
+
+        #region implemented abstract members of Packet
+
+        public override byte[] Build()
+        {
+            return new byte[0];
+        }
+
+        public override PolarisServer.Models.PacketHeader GetHeader()
+        {
+            return new PolarisServer.Models.PacketHeader
+            {
+                type = type,
+                subtype = subtype
+            };
+        }
+
+        #endregion
+    }
+}
+
