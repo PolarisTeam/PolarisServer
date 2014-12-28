8dfed554349817c382d8731a85bdc50a5083c205
Sean CyberKitsune McClenaghan <cyberkitsune09@gmail.com>
2014-12-27 21:40:03 -0800

    I'm dumb, do this. Fixes build.

--- PolarisServer/Database/PolarisEF.cs
+++ PolarisServer/Database/PolarisEF.cs
@@ -50,7 +50,7 @@ namespace PolarisServer.Database
                     this.ServerInfos.Add(revision);
 
                     //TODO Possibly move this somewhere else?
-                    this.Database.SqlQuery("ALTER TABLE Players AUTO_INCREMENT=@p0", 100000000);
+                    this.Database.ExecuteSqlCommand("ALTER TABLE Players AUTO_INCREMENT=100000000");
                 }
                 this.SaveChanges();
 
