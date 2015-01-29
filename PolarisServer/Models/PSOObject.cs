74224ae70895334db6eb1ead5d24e1f1fdce32aa
Sean CyberKitsune McClenaghan <cyberkitsune09@gmail.com>
2015-01-28 23:26:02 -0800

    Add PSOObject Class

---
+++ PolarisServer/Models/PSOObject.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PolarisServer.Models
+{
+    class PSOObject
+    {
+        class PSOObjectThing
+        {
+            UInt32 data;
+        }
+
+        int ObjectID { get; set; }
+        MysteryPositions Position { get; set; }
+        string Name;
+        PSOObjectThing[] things;
+
+    }
+}
