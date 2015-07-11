4e1aed6ddeede154db5dfad32eaef0c0677f81cd
Sean "CyberKitsune" McClenaghan <cyberkitsune09@gmail.com>
2015-07-11 15:48:32 -0700

    Began work on ObjectManager. Lobby Spawning code now uses this.

---
+++ PolarisServer/Object/ObjectManager.cs
@@ -0,0 +1,66 @@
+﻿using Newtonsoft.Json;
+using PolarisServer.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace PolarisServer.Object
+{
+    class ObjectManager
+    {
+        private static readonly ObjectManager instance = new ObjectManager();
+
+        private Dictionary<String, Dictionary<uint, PSOObject>> zoneObjects = new Dictionary<string, Dictionary<uint, PSOObject>>();
+
+        private ObjectManager() {}
+
+        public static ObjectManager Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        public Dictionary<uint, PSOObject> getObjectsForZone(string zone)
+        {
+            if(!zoneObjects.ContainsKey(zone))
+            {
+                //TODO Maybe make some resource management class for this stuff?
+                if(!Directory.Exists("Resources/objects/" + zone))
+                {
+                    throw new Exception(String.Format("Unable to get objects for Zone {0}, Object folder not present.", zone));
+                }
+
+                Dictionary<uint, PSOObject> objects = new Dictionary<uint, PSOObject>();
+                var objectPaths = Directory.GetFiles("Resources/objects/" + zone);
+                Array.Sort(objectPaths);
+                foreach (var path in objectPaths)
+                {
+                    if (Path.GetExtension(path) == ".bin")
+                    {
+                        var newObject = PSOObject.FromPacketBin(File.ReadAllBytes(path));
+                        objects.Add(newObject.Header.ID, newObject);
+                        Logger.WriteInternal("[OBJ] Loaded object ID {0} with name {1} pos: ({2}, {3}, {4})", newObject.Header.ID, newObject.Name, newObject.Position.X,
+                            newObject.Position.Y, newObject.Position.Z);
+                    }
+                    else if (Path.GetExtension(path) == ".json")
+                    {
+                        var newObject = JsonConvert.DeserializeObject<PSOObject>(File.ReadAllText(path));
+                        objects.Add(newObject.Header.ID, newObject);
+                        Logger.WriteInternal("[OBJ] Loaded object ID {0} with name {1} pos: ({2}, {3}, {4})", newObject.Header.ID, newObject.Name, newObject.Position.X,
+                            newObject.Position.Y, newObject.Position.Z);
+                    }
+                }
+
+                zoneObjects.Add(zone, objects);
+                return objects;
+
+            }
+            else
+            {
+                return zoneObjects[zone];
+            }
+        }
+    }
+}
