d467b28886b227a19b6a0ae5cfced553655e5762
Sean "CyberKitsune" McClenaghan <cyberkitsune09@gmail.com>
2015-07-12 14:10:14 -0700

    Forgot this.

---
+++ PolarisServer/Packets/PSOPackets/MovementPacket.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PolarisServer.Models;
+
+namespace PolarisServer.Packets.PSOPackets
+{
+    class MovementPacket : Packet
+    {
+        public override byte[] Build()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override PacketHeader GetHeader()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
