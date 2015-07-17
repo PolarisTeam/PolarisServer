530ba5845f02357331bade2212378a7f7bfb9b0b
Sean "CyberKitsune" McClenaghan <cyberkitsune09@gmail.com>
2015-07-16 20:03:12 -0700

    Add these (Incomplete)

---
+++ PolarisServer/Packets/Handlers/SymbolArtHandler.cs
@@ -0,0 +1,18 @@
+﻿using PolarisServer.Packets.PSOPackets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PolarisServer.Packets.Handlers
+{
+
+    [PacketHandlerAttr(0x2F, 0x6)]
+    class SymbolArtListHandler : PacketHandler
+    {
+        public override void HandlePacket(Client context, byte flags, byte[] data, uint position, uint size)
+        {
+            context.SendPacket(new SymbolArtList(new Models.ObjectHeader((uint)context.User.PlayerId, Models.EntityType.Player)));
+        }
+    }
+}
