62137e1291c5b80e4ced59164dea9ad9d51bdcc3
Treeki <treeki@gmail.com>
2015-07-17 06:50:02 +0200

    use a queue when sending stuff to clients, to avoid blocking on send

--- PolarisServer/Network/SocketClient.cs
+++ PolarisServer/Network/SocketClient.cs
@@ -8,8 +8,9 @@ namespace PolarisServer.Network
 
         public delegate void DataReceivedDelegate(byte[] data, int size);
 
-        private readonly byte[] _readBuffer;
+        private readonly byte[] _readBuffer, _writeBuffer;
         private readonly SocketServer _server;
+        private int _writePosition = 0;
 
         public SocketClient(SocketServer server, TcpClient socket)
         {
@@ -17,11 +18,14 @@ namespace PolarisServer.Network
             Socket = socket;
 
             _readBuffer = new byte[1024 * 16];
+            _writeBuffer = new byte[1024 * 1024]; // too high? too low? not sure
         }
 
         public TcpClient Socket { get; private set; }
         public event DataReceivedDelegate DataReceived;
         public event ConnectionLostDelegate ConnectionLost;
+
+        public bool NeedsToWrite { get { return (_writePosition > 0); } }
 
         public bool OnReadable()
         {
@@ -46,6 +50,44 @@ namespace PolarisServer.Network
                 _server.NotifyConnectionClosed(this);
                 return false;
             }
+        }
+
+        public bool OnWritable()
+        {
+            try
+            {
+                var write = Socket.Client.Send(_writeBuffer, 0, _writePosition, SocketFlags.None);
+                if (write == 0)
+                {
+                    // Connection failed, presumably
+                    ConnectionLost();
+                    _server.NotifyConnectionClosed(this);
+                    return false;
+                }
+
+                System.Array.Copy(_writeBuffer, write, _writeBuffer, 0, _writePosition - write);
+                _writePosition -= write;
+
+                return true;
+            }
+            catch (SocketException)
+            {
+                ConnectionLost();
+                _server.NotifyConnectionClosed(this);
+                return false;
+            }
+        }
+
+        public void Write(byte[] blob)
+        {
+            if ((_writePosition + blob.Length) > _writeBuffer.Length)
+            {
+                // Buffer exceeded!
+                throw new System.Exception("too much data in write queue");
+            }
+
+            System.Array.Copy(blob, 0, _writeBuffer, _writePosition, blob.Length);
+            _writePosition += blob.Length;
         }
 
         public void Close()
