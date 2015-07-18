cb0fac0eb699ece00d42f69b951f19387efdf228
Sean "CyberKitsune" McClenaghan <cyberkitsune09@gmail.com>
2015-07-17 18:21:52 -0700

    Set memory packet already has header

--- PolarisServer/Packets/Handlers/CharacterSpawn.cs
+++ PolarisServer/Packets/Handlers/CharacterSpawn.cs
@@ -39,15 +39,15 @@ namespace PolarisServer.Packets.Handlers
             // Unlock Controls
             context.SendPacket(new NoPayloadPacket(0x03, 0x2B));
 
-            // memset packet - Enables menus
-            // Also holds event items and likely other stuff too
-            var memSetPacket = File.ReadAllBytes("Resources/setMemoryPacket.bin");
-            context.SendPacket(0x23, 0x07, 0, memSetPacket);
-
             //context.SendPacket(File.ReadAllBytes("testbed/237.23-7.210.189.208.30.bin"));
 
             // Give a blank palette
             context.SendPacket(new PalettePacket());
+
+            // memset packet - Enables menus
+            // Also holds event items and likely other stuff too
+            var memSetPacket = File.ReadAllBytes("Resources/setMemoryPacket.bin");
+            context.SendPacket(memSetPacket);
         }
 
         #endregion
