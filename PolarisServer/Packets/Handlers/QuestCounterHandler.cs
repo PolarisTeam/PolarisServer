092ac3431b136d4aeb7aba8213a6f48219d85bae
Kyle Belanger <kyle873@gmail.com>
2015-08-15 03:52:43 -0400

    Quest start packet stuff

--- PolarisServer/Packets/Handlers/QuestCounterHandler.cs
+++ PolarisServer/Packets/Handlers/QuestCounterHandler.cs
@@ -80,4 +80,37 @@ namespace PolarisServer.Packets.Handlers
             context.SendPacket(new NoPayloadPacket(0xB, 0x1C));
         }
     }
+
+    // TODO: Attach the right packethandler
+    // [PacketHandlerAttr(0xB, 0x..)]
+    class QuestDifficultyStartHandler : PacketHandler
+    {
+        // Go go maximum code duplication (for now)
+        public override void HandlePacket(Client context, byte flags, byte[] data, uint position, uint size)
+        {
+            QuestListPacket.QuestDefiniton def = new QuestListPacket.QuestDefiniton();
+            def.dateOrSomething = "2012/01/05";
+            def.needsToBeNonzero = 0x00000020;
+            def.getsSetToWord = 0x0000000B;
+            def.questNameString = 30010;
+            def.playTime = (byte)QuestListPacket.EstimatedTime.Short;
+            def.partyType = (byte)QuestListPacket.PartyType.SinglePartyQuest;
+            def.difficulties = (byte)QuestListPacket.Difficulties.Normal | (byte)QuestListPacket.Difficulties.hard | (byte)QuestListPacket.Difficulties.VeryHard | (byte)QuestListPacket.Difficulties.SuperHard;
+            def.requiredLevel = 1;
+            // Not sure why but these need to be set for the quest to be enabled
+            def.field_FF = 0xF1;
+            def.field_101 = 1;
+
+            QuestDifficultyPacket.QuestDifficulty diff = new QuestDifficultyPacket.QuestDifficulty();
+            diff.dateOrSomething = "2012/01/05";
+            diff.something = 0x20;
+            diff.something2 = 0x0B;
+            diff.questNameString = 30010;
+
+            // These are likely bitfields
+            diff.something3 = 0x00030301;
+
+            context.SendPacket(new QuestStartPacket(def, diff));
+        }
+    }
 }
---
+++ PolarisServer/Packets/PSOPackets/QuestStartPacket.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+using PolarisServer.Models;
+
+namespace PolarisServer.Packets.PSOPackets
+{
+    class QuestStartPacket : Packet
+    {
+        QuestListPacket.QuestDefiniton data;
+        QuestDifficultyPacket.QuestDifficulty difficulty;
+
+        public QuestStartPacket(QuestListPacket.QuestDefiniton data, QuestDifficultyPacket.QuestDifficulty difficulty)
+        {
+            this.data = data;
+            this.difficulty = difficulty;
+        }
+
+        public override byte[] Build()
+        {
+            PacketWriter writer = new PacketWriter();
+
+            writer.Write(0x753A); // Magic, needs proper sub/xor
+            writer.Write((int)0);
+            writer.WriteStruct<QuestListPacket.QuestDefiniton>(data);
+            writer.WriteStruct<QuestDifficultyPacket.QuestDifficulty>(difficulty);
+
+            return writer.ToArray();
+        }
+
+        public override PacketHeader GetHeader()
+        {
+            return new PacketHeader(0xB, 0x31, 0x4);
+        }
+    }
+}
