6804206aaf2202a907d5c11b56e6fdd06246ed9c
Kyle Belanger <kyle873@gmail.com>
2015-08-19 04:01:01 -0400

    Probably not working handler for getting off the campship

--- PolarisServer/Packets/Handlers/CampshipTeleport.cs
+++ PolarisServer/Packets/Handlers/CampshipTeleport.cs
@@ -24,4 +24,24 @@ namespace PolarisServer.Packets.Handlers
             campship.SpawnClient(context, new PSOLocation(0, 1, 0, 0, 0, 0, 0), context.currentParty.currentQuest.name);
         }
     }
+
+    [PacketHandlerAttr(0x4, 0x13)]
+    class CampshipTeleportDown : PacketHandler
+    {
+        public override void HandlePacket(Client context, byte flags, byte[] data, uint position, uint size)
+        {
+            if (context.currentParty.currentQuest == null)
+                return;
+
+            // TODO: WTF terribad hax?
+            if (context.CurrentLocation.PosZ >= 20)
+            {
+                var instanceName = String.Format("{0}-{1}", context.currentParty.currentQuest.name, context.User.Nickname);
+                ZoneManager.Instance.NewInstance(instanceName, new Map("forest", 310, 0, Map.MapType.Quest, Map.MapFlags.EnableMap));
+
+                Map forest = ZoneManager.Instance.MapFromInstance("campship", instanceName);
+                forest.SpawnClient(context, new PSOLocation(0, 1, 0, 0, 0, 0, 0));
+            }
+        }
+    }
 }
